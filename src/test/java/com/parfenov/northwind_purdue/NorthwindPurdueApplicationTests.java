e2c1818119be5051165f9c28845b4671760485dd
Dmitrii Parfenov <parfenovds@protonmail.com>
2024-10-04 09:28:22 +0300

    initial commit

---
+++ src/test/java/com/parfenov/northwind_purdue/NorthwindPurdueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parfenov.northwind_purdue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindPurdueApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
